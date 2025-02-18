b5f898dd5d7a0d01b9d70c9053bb5cc17a571fbf
ntvorup <nach0004@stud.kea.dk>
2025-02-18 10:45:36 +0100

    first commit

---
+++ src/test/java/com/example/gits/GitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
